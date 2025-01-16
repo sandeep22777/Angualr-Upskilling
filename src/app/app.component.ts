36d91920504c993c30571ad4215defc61cac86e9
sandeep22777 <sandeepdeepu22777@gmail.com>
2025-01-16 17:49:46 +0530

    Updated Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,16 +20,39 @@ export interface ITodo {
 export class AppComponent {
   title: string = 'BestBuy';
   check: string = 'checkbox';
+  data1: number = 0;
   temp: number = 0;
+
   twoWay: string = '';
 
   count: number = 0;
 
   increment() {
+    this.temp = this.data1;
     this.count += this.temp;
   }
 
   increaseTemp(event: any) {
-    this.temp = +event.target.value;
+    this.data1 = +event.target.value;
+  }
+
+  increaseOne() {
+    this.count += 1;
+  }
+
+  decrementOne() {
+    this.count -= 1;
+  }
+
+  todos: ITodo[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<ITodo[]>('https://jsonplaceholder.typicode.com/todos')
+      .subscribe((data) => {
+        this.todos = data;
+      });
   }
 }
