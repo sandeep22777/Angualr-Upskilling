a80abd53e400540c043a020841394749933096fb
sandeep22777 <sandeepdeepu22777@gmail.com>
2025-01-16 16:27:58 +0530

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface ITodo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-upskilling';
+  title: string = 'BestBuy';
+  check: string = 'checkbox';
+  temp: number = 0;
+  twoWay: string = '';
+
+  count: number = 0;
+
+  increment() {
+    this.count += this.temp;
+  }
+
+  increaseTemp(event: any) {
+    this.temp = +event.target.value;
+  }
 }
